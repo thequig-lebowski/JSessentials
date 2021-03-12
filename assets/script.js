6d3703a88f5d8e08219007b7a7ceed7b979606be
David O'Quigley <oquigld2@gmail.com>
2021-03-12 18:00:14 +0000

    Added functions, random numbers and addition game

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,39 @@
-function runGame(){
+//Wait for DOM to load before running the game
+//Get the button elements and add eventlisteners to them
 
+window.addEventListener('DOMContentLoaded', function(){
+	let buttons = document.getElementsByTagName('button');
+
+	for (let button of buttons){
+		button.addEventListener('click', function(){
+			if (this.getAttribute('data-type') === 'submit') {
+				alert('You clicked Submit!');
+			} else {
+				let gameType = this.getAttribute('data-type');
+				runGame(gameType);
+			}
+		});
+	}
+
+	runGame('addition');
+});
+
+
+
+function runGame(gameType){
+	
+	//Generate two random no's between 1 and 25, Math.floor rounds down to whole numbers
+	// the '+1' is to compensate for the cases where we round down to zero
+
+	let num1 = Math.floor(Math.random() * 25) + 1;	 
+	let num2 = Math.floor(Math.random() * 25) + 1;
+
+	if (gameType === 'addition'){
+		displayAdditionQuestion(num1, num2);
+	} else {
+		alert(`Unknown game type ${gameType}`);
+		throw `Unknown game type ${gameType}, aborting`;
+	}
 
 }
 
@@ -23,8 +57,11 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
 
+	document.getElementById('operand1').textContent = operand1;
+	document.getElementById('operand2').textContent = operand2;
+	document.getElementById('operator').textContent = '+';		
 
 }
 
@@ -35,5 +72,6 @@ function displaySubtractQuestion(){
 
 function displayMultiplyQuestion(){
 
-	
+
 }
+
